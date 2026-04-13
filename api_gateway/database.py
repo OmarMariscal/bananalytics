adcc2b2dd607a31fa2087b0bbf8d1fff84188b20
Enjeru-105 <gomez.a011005@gmail.com>
2026-04-13 15:36:58 -0600

    feat: Creación y optimización ORM de Base de datos completa en Neon a través de python y SQLAlchemy  (#5)
    * feat: Creación de Base de datos
    
    * refactor: implementación de relaciones explícitas e índices para facilitar el trabajo de Machine Learning

---
+++ api_gateway/database.py
@@ -0,0 +1,24 @@
+import os
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+from models import Base  # Importa las tablas de models.py
+
+#Cargar la URL de conexión desde el .env
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+#Crear el "motor" que se conecta a Neon
+engine = create_engine(DATABASE_URL, echo=True)
+
+#Función para generar las tablas
+def init_db():
+    print("Conectando a Neon y borrando tablas viejas...")
+    Base.metadata.drop_all(bind=engine) #ELiminamos tablas viejas para crearlas de nuevo con las nuevas características
+
+    print("Creando tablas nuevas con la arquitectura actualizada...")
+    #traducción de clases de Python a SQL y las ejecuta en Neon
+    Base.metadata.create_all(bind=engine)
+    print("¡Tablas creadas exitosamente!")
+
+if __name__ == "__main__":
+    init_db()
