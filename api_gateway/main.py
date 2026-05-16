c7867d054ebdb6ecafe1180c437537c6206a03c4
Enjeru-105 <gomez.a011005@gmail.com>
2026-05-16 14:53:40 -0600

    fix: corrección de error de typo

--- api_gateway/main.py
+++ api_gateway/main.py
@@ -391,7 +391,7 @@ def get_predictions(store_id: int, db: Session = Depends(get_db), token_store_id
         respuesta.append({
             "barcode": pred.barcode,
             "product_name": pred.product_name,
-            "Category": pred.category,
+            "category": pred.category,
             "image_url": pred.image_url,
             "objective_date": pred.objective_date.strftime("%Y-%m-%d"), #Convertimos la fecha a texto
             "prediction": pred.prediction,
